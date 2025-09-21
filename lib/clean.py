eeb1cc566cdeba79e451e878f2a59dbd9205e4e0
Ashan Pradeep <aapkumarasiri99@gmail.com>
2025-09-21 14:59:52 +0530

    Delete lib directory

--- lib/clean.py
+++
@@ -1,4 +0,0 @@
-import re
-
-def strip_think(text: str) -> str:
-    return re.sub(r"<think>.*?</think>", "", text, flags=re.DOTALL).strip()
--- lib/split_docs.py
+++
@@ -1,5 +0,0 @@
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-
-def split_docs(docs):
-    splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=150)
-    return splitter.split_documents(docs)
